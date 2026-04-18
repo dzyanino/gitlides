584310a2502017a667bf64ff9d46f6f3470551b8
dzyanino <dzyanino@gmail.com>
2026-04-18 23:29:15 +0300

    feat: added OAauth login/logout with Github

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,20 +1,11 @@
 <script setup lang="ts">
-import type { ButtonProps } from '@nuxt/ui'
+const { loggedIn, clear } = useUserSession()
 
-const links = ref<ButtonProps[]>([
-  {
-    label: 'Login with Github',
-    icon: 'i-simple-icons-github',
-    color: 'neutral'
-  },
-  {
-    label: 'Learn more',
-    to: '/docs/getting-started/theme/design-system',
-    color: 'neutral',
-    variant: 'subtle',
-    trailingIcon: 'i-lucide-arrow-right'
+watch(loggedIn, (yes) => {
+  if (!yes) {
+    navigateTo('/')
   }
-])
+})
 </script>
 
 <template>
@@ -22,7 +13,44 @@ const links = ref<ButtonProps[]>([
     title="You can go blabla"
     description="For blabla and blablabla, blabla bla blablabla."
     headline="Hehe"
-    :links="links"
     class="w-full h-full"
-  />
+  >
+    <template #links>
+      <UButton
+        v-if="!loggedIn"
+        to="/api/auth/github"
+        icon="i-simple-icons-github"
+        label="Login with GitHub"
+        color="neutral"
+        size="lg"
+        external
+      />
+
+      <template v-else>
+        <UButton
+          to="/home"
+          icon="i-lucide-git-graph"
+          label="Continue"
+          color="neutral"
+          size="lg"
+        />
+        <UButton
+          icon="i-lucide-log-out"
+          label="Logout"
+          color="neutral"
+          variant="subtle"
+          size="lg"
+          @click="clear"
+        />
+      </template>
+
+      <UButton
+        to="#"
+        label="Learn more"
+        color="neutral"
+        variant="subtle"
+        size="lg"
+      />
+    </template>
+  </UPageHero>
 </template>
