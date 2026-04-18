3f3d8666b407321c8ccdbff5bebae34fcb85600e
dzyanino <dzyanino@gmail.com>
2026-04-18 22:17:05 +0300

    feat(layouts): added default layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <UApp>
+    <UHeader title="Gitlides">
+      <template #right>
+        <UColorModeButton />
+        <UButton
+          to="https://github.com/nuxt-ui-templates/starter"
+          target="_blank"
+          icon="i-simple-icons-github"
+          aria-label="GitHub"
+          color="neutral"
+          variant="ghost"
+        />
+      </template>
+    </UHeader>
+
+    <UMain>
+      <slot />
+    </UMain>
+  </UApp>
+</template>
