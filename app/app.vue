51d5d4c2d61c104e9122e5194f492cb6a6c9861e
dzyanino <dzyanino@gmail.com>
2026-04-18 21:36:51 +0300

    Initial commit

---
+++ app/app.vue
@@ -0,0 +1,31 @@
+<script setup>
+useHead({
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+  ],
+  link: [
+    { rel: 'icon', href: '/favicon.ico' }
+  ],
+  htmlAttrs: {
+    lang: 'en'
+  }
+})
+
+const title = 'Gitlides'
+const description = 'For sliding you github repos.'
+
+useSeoMeta({
+  title,
+  description,
+  ogTitle: title,
+  ogDescription: description,
+  ogImage: 'https://avatars.githubusercontent.com/u/111046359?v=4',
+  twitterCard: 'summary_large_image'
+})
+</script>
+
+<template>
+  <UApp>
+    <NuxtPage />
+  </UApp>
+</template>
