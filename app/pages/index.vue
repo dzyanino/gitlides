f21967ed961e47d06df0988041699b29f576b76d
dzyanino <dzyanino@gmail.com>
2026-04-18 22:17:59 +0300

    feat(pages): dummy hero

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,28 @@
+<script setup lang="ts">
+import type { ButtonProps } from '@nuxt/ui'
+
+const links = ref<ButtonProps[]>([
+  {
+    label: 'Login with Github',
+    icon: 'i-simple-icons-github',
+    color: 'neutral'
+  },
+  {
+    label: 'Learn more',
+    to: '/docs/getting-started/theme/design-system',
+    color: 'neutral',
+    variant: 'subtle',
+    trailingIcon: 'i-lucide-arrow-right'
+  }
+])
+</script>
+
 <template>
-  <div>
-    test
-  </div>
+  <UPageHero
+    title="You can go blabla"
+    description="For blabla and blablabla, blabla bla blablabla."
+    headline="Hehe"
+    :links="links"
+    class="w-full h-full"
+  />
 </template>
