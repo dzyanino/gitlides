63e09cd7f9b9c23674b32f8bff953c54b74dbdbe
dzyanino <dzyanino@gmail.com>
2026-04-18 23:57:42 +0300

    feat(api): changed auth scope to public repo

--- server/api/auth/github.get.ts
+++ server/api/auth/github.get.ts
@@ -1,4 +1,7 @@
 export default defineOAuthGitHubEventHandler({
+  config: {
+    scope: ['public_repo']
+  },
   async onSuccess(event, { user }) {
     await setUserSession(event, { user })
     return sendRedirect(event, '/home')
