9a1e96b55b257ac180d3cb6027344b762e0f76ff
dzyanino <dzyanino@gmail.com>
2026-04-21 19:17:28 +0300

    feat(pages): added user detection to redirect after login

--- app/pages/confirm.vue
+++ app/pages/confirm.vue
@@ -1,3 +1,23 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: false
+})
+
+const user = useSupabaseUser()
+
+watch(user, () => {
+  if (user.value) {
+    return navigateTo('/home')
+  }
+}, { immediate: true })
+</script>
+
 <template>
-  <div>confirm</div>
+  <UApp>
+    <UMain class="flex flex-col flex-1">
+      <div class="flex flex-1 items-center justify-center p-4">
+        <span>Waiting for login...</span>
+      </div>
+    </UMain>
+  </UApp>
 </template>
