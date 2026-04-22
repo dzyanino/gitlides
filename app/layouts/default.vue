836ede0b609d5861d3765c1df00a0285a0260b11
dzyanino <dzyanino@gmail.com>
2026-04-22 14:59:46 +0300

    feat(layout): better logout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,11 +1,37 @@
 <script lang="ts" setup>
-const supabase = useSupabaseClient()
+import type { DropdownMenuItem } from '@nuxt/ui'
+
 const user = useSupabaseUser()
+const supabase = useSupabaseClient()
+
+const isLogOutModalOpen = shallowRef<boolean>(false)
+
+const items = ref<DropdownMenuItem[][]>([
+  [
+    {
+      label: `${(user.value && user.value.user_metadata) ? user.value.user_metadata.name : 'no user'}`,
+      description: `${(user.value && user.value.user_metadata) ? user.value.user_metadata.user_name : 'login first'}`,
+      type: 'label'
+    }
+  ],
+  [
+    {
+      label: 'Logout',
+      icon: 'i-lucide-log-out',
+      color: 'error',
+      onSelect: () => isLogOutModalOpen.value = true
+    }
+  ]
+])
 
 async function signOut() {
   const { error } = await supabase.auth.signOut()
 
   if (error) console.error(error)
+
+  isLogOutModalOpen.value = false
+
+  navigateTo('/login')
 }
 </script>
 
@@ -27,20 +53,53 @@ async function signOut() {
           variant="ghost"
         />
 
-        <UButton
-          v-if="user"
-          icon="i-lucide-log-out"
-          :label="user.email"
-          color="neutral"
-          variant="subtle"
-          size="lg"
-          @click="signOut"
-        />
+        <template v-if="user">
+          <UDropdownMenu
+            :items
+            arrow
+          >
+            <UButton
+              v-bind="
+                user.user_metadata
+                  ? {
+                    avatar: {
+                      src: `${user.user_metadata.avatar_url}`,
+                      loading: 'lazy'
+                    }
+                  }
+                  : { icon: 'i-lucide-circle-user' }
+              "
+              color="neutral"
+              variant="ghost"
+            />
+          </UDropdownMenu>
+        </template>
       </template>
     </UHeader>
 
     <UMain class="flex">
       <slot />
     </UMain>
+
+    <UModal
+      v-model:open="isLogOutModalOpen"
+      title="Wait"
+      description="You'll need to login again"
+      :ui="{ footer: 'justify-end' }"
+    >
+      <template #footer="{ close }">
+        <UButton
+          label="Cancel"
+          color="neutral"
+          variant="outline"
+          @click="close"
+        />
+        <UButton
+          label="Confirm"
+          color="error"
+          @click="signOut"
+        />
+      </template>
+    </UModal>
   </UApp>
 </template>
