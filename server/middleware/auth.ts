1d9ebcb73c9accaaf59b409aa3ebcfc314df4d86
dzyanino <dzyanino@gmail.com>
2026-04-25 01:12:09 +0300

    feat(middleware): added server middleware for supabase authentication checking

---
+++ server/middleware/auth.ts
@@ -0,0 +1,7 @@
+import { serverSupabaseUser } from '#supabase/server'
+
+export default defineEventHandler(async (event) => {
+  const user = await serverSupabaseUser(event)
+  if (!user)
+    throw createError({ statusCode: 401, message: 'Unauthorized' })
+})
