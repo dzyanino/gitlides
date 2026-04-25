154f2e46a3a74b1b737ef3b3352ca0abb7f8b800
dzyanino <dzyanino@gmail.com>
2026-04-25 18:09:10 +0300

    feat(utils): moved github token retriever to an util

---
+++ server/utils/get-github-token.ts
@@ -0,0 +1,21 @@
+import type { H3Event } from '#imports'
+import { serverSupabaseClient } from '#supabase/server'
+
+export async function getGithubToken(event: H3Event<globalThis.EventHandlerRequest>) {
+  const { userId } = await requireAuth(event)
+
+  const supabase = await serverSupabaseClient(event)
+
+  const { data: tokenData, error: tokenError } = await supabase
+    .from('provider_token')
+    .select('token')
+    .eq('user_id', userId)
+
+  if (tokenError)
+    throw createError({ statusCode: 500, message: tokenError.message })
+
+  if (!tokenData[0] || !tokenData[0].token)
+    throw createError({ statusCode: 404, message: 'No token found' })
+
+  return { userId, token: tokenData[0].token }
+}
