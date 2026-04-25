1a26e1e39e269e67a25de8b1e1fe8dd0b4d3d454
dzyanino <dzyanino@gmail.com>
2026-04-25 16:56:49 +0300

    feat(pages): better section layout that fixes content and scrolls when needed

--- app/pages/home.vue
+++ app/pages/home.vue
@@ -1,4 +1,9 @@
 <script lang="ts" setup>
+// import { Octokit } from 'octokit'
+
+// const session = useSupabaseSession()
+// const user = useSupabaseUser()
+// const octokit = new Octokit({ auth: session.value?.access_token })
 </script>
 
 <template>
@@ -16,17 +21,16 @@
     class="max-w-md mx-auto self-center -mt-(--ui-header-height)"
   />
 
-  <UContainer
-    v-else
-    class="py-4"
-  >
-    <div class="flex flex-col md:flex-row gap-4 size-full justify-evenly">
-      <div class="flex flex-col flex-1/3 justify-start bg-primary/50">
+  <UContainer v-else>
+    <div class="flex flex-col md:flex-row gap-4 w-full h-[calc(100vh-var(--ui-header-height))] justify-evenly py-4">
+      <div class="flex flex-col flex-1/3 max-h-full overflow-auto wrap-break-word justify-start bg-primary/10">
         <span>LEFT</span>
       </div>
 
-      <div class="flex flex-col flex-2/3 justify-start bg-primary/50">
-        <HomeCommitsList />
+      <div class="flex flex-col flex-2/3 max-h-full overflow-auto wrap-break-word justify-start bg-primary/10">
+        <!-- <HomeCommitsList /> -->
+        <!-- <span>{{ session }}</span> -->
+        <HomeReposList />
       </div>
     </div>
   </UContainer>
