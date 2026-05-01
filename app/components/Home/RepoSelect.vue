1ecaebd96b08fe459334c3411dd586a82f64bf88
dzyanino <dzyanino@gmail.com>
2026-05-01 18:28:38 +0300

    feat(components): added repo list for both mobile and desktop

---
+++ app/components/Home/RepoSelect.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+const selectedRepo = inject('selectedRepo', shallowRef<GithubRepo>())
+
+const { data: repos, pending: reposLoading } = await useFetch('/api/github/saved-repo')
+</script>
+
+<template>
+  <div class="flex flex-col w-full items-center justify-center">
+    <!-- @vue-ignore -->
+    <USelectMenu
+      v-model="selectedRepo"
+      :items="repos"
+      :loading="reposLoading"
+      clear
+
+      :search-input="{ icon: 'i-lucide-search' }"
+
+      label-key="full_name"
+      class="w-full"
+    />
+  </div>
+</template>
--- app/components/Home/ReposList.vue
+++ app/components/Home/ReposList.vue
@@ -1,7 +1,30 @@
 <script lang="ts" setup>
-const { data: repos } = await useFetch('/api/github/saved-repo')
+const selectedRepo = inject('selectedRepo', shallowRef<GithubRepo>())
+
+const { data: repos, pending: reposLoading } = await useFetch('/api/github/saved-repo')
 </script>
 
 <template>
-  <div>{{ repos }}</div>
+  <div class="flex flex-col w-full h-full md:h-4/6 md:max-h-4/6 items-start justify-start">
+    <!-- @vue-ignore -->
+    <UListbox
+      v-model="selectedRepo"
+
+      :items="repos"
+      :loading="reposLoading"
+
+      :filter="{ icon: 'i-lucide-search' }"
+
+      label-key="full_name"
+      class="size-full"
+    >
+      <template #item-label="{ item }">
+        <span>{{ item.full_name }}</span>
+      </template>
+
+      <template #item-description="{ item }">
+        <span>{{ item.description }}</span>
+      </template>
+    </UListbox>
+  </div>
 </template>
