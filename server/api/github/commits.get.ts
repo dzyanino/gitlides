db1a9686b2d804b49c3f8afcb653311f7dcbd3da
dzyanino <dzyanino@gmail.com>
2026-05-03 22:07:01 +0300

    feat(api): transformed and grouped commit data by date before returning it

--- server/api/github/commits.get.ts
+++ server/api/github/commits.get.ts
@@ -1,5 +1,37 @@
 import { Octokit, RequestError } from 'octokit'
 
+function transformAndGroupCommits(commits: GitHubCommit[]): CommitGroup[] {
+  const simplified: TransformedCommit[] = commits.map(commit => ({
+    sha: commit.sha,
+    html_url: commit.html_url,
+    message: commit.commit.message,
+    author: {
+      name: commit.commit.author.name,
+      email: commit.commit.author.email,
+      login: commit.author?.login,
+      avatar_url: commit.author?.avatar_url
+    },
+    date: commit.commit.author.date,
+    verified: commit.commit.verification.verified
+  }))
+
+  const grouped = simplified.reduce<Record<string, TransformedCommit[]>>((acc, commit) => {
+    const day = commit.date.split('T')[0]
+
+    if (day) {
+      if (!acc[day]) acc[day] = []
+
+      acc[day].push(commit)
+    }
+
+    return acc
+  }, {})
+
+  return Object.entries(grouped)
+    .map(([date, commits]): CommitGroup => ({ date, commits }))
+    .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+}
+
 export default defineEventHandler(async (event) => {
   const { token: auth } = await getGithubToken(event)
 
@@ -13,14 +45,18 @@ export default defineEventHandler(async (event) => {
   try {
     const octokit = new Octokit({ auth })
 
-    const { data } = await octokit.rest.repos.listCommits({
+    const { data } = await octokit.request('GET /repos/{owner}/{repo}/commits', {
       owner: owner.toString(),
-      repo: repo.toString()
+      repo: repo.toString(),
+      headers: {
+        'X-GitHub-Api-Version': '2026-03-10'
+      }
     })
 
-    return data
+    return transformAndGroupCommits(data as GitHubCommit[])
   } catch (error) {
-    if (error instanceof RequestError)
+    if (error instanceof RequestError) {
       throw createError({ status: 500, message: 'Error fetching repos' })
+    }
   }
 })
