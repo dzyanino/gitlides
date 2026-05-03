574293aec9d38359be88c347c82986559998d4af
dzyanino <dzyanino@gmail.com>
2026-05-03 22:41:08 +0300

    feat(components): added component to show list of commits with a given date

---
+++ app/components/Home/DatedCommits.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+defineProps<{
+  group: CommitGroup
+}>()
+</script>
+
+<template>
+  <UCard
+    :title="group.date"
+  >
+    <ul class="list-disc list-inside">
+      <template
+        v-for="(commit, index) in group.commits"
+        :key="commit.sha"
+      >
+        <li class="text-md font-bold">
+          {{ commit.message }}
+        </li>
+        <span class="indent-6 line-clamp-1">{{ commit.sha }}</span>
+        <span class="indent-6 line-clamp-1">{{ commit.author.login }}</span>
+        <span
+          class="indent-6 line-clamp-1"
+          :class="{ 'mb-4': index < (group.commits.length - 1) }"
+        >
+          {{ commit.verified }}
+        </span>
+      </template>
+    </ul>
+  </UCard>
+</template>
