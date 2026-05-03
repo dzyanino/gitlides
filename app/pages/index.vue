c91326f603f781e0780e51156ce7d3622765d0fa
dzyanino <dzyanino@gmail.com>
2026-05-03 17:52:13 +0300

    feat(pages): hero section with font style and aniamted background

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,36 +3,61 @@ const user = useSupabaseUser()
 </script>
 
 <template>
-  <UPageHero
-    title="No more boring meetings"
-    description="Gitlides is a tool for making slides out of daily commits to use during review meetings."
-    class="w-full h-full"
-  >
-    <template #links>
-      <UButton
-        v-if="!user"
-        to="/login"
-        icon="i-lucide-arrow-right"
-        label="Get started"
-        size="lg"
-      />
+  <div class="flex-1">
+    <Teleport to="body">
+      <IndexBackground
+        direction="left"
+        :speed="0.1"
+        :square-size="40"
+        hover-fill-color="#27ff64"
 
-      <UButton
-        v-else
-        to="/home"
-        icon="i-lucide-git-graph"
-        label="Continue"
-        size="lg"
-        color="neutral"
+        class="fixed inset-0 -z-10"
       />
+    </Teleport>
 
-      <UButton
-        to="#"
-        label="Learn more"
-        color="neutral"
-        variant="subtle"
-        size="lg"
-      />
-    </template>
-  </UPageHero>
+    <UPageHero
+      description="Gitlides is a tool for making slides out of daily commits to use during review meetings."
+      class="w-full h-full"
+    >
+      <template #title>
+        <span class="main-title">No more <span class="title-emphasis text-dimmed">boring</span> meetings</span>
+      </template>
+
+      <template #links>
+        <UButton
+          v-if="!user"
+          to="/login"
+          icon="i-lucide-arrow-right"
+          label="Get started"
+          size="lg"
+        />
+
+        <UButton
+          v-else
+          to="/home"
+          icon="i-lucide-git-graph"
+          label="Continue"
+          size="lg"
+        />
+
+        <UButton
+          to="#"
+          label="Learn more"
+          color="neutral"
+          variant="subtle"
+          size="lg"
+        />
+      </template>
+    </UPageHero>
+  </div>
 </template>
+
+<style scoped>
+.main-title {
+  font-family: 'Arima', serif;
+}
+
+.title-emphasis {
+  font-family: 'Rock 3D', sans-serif;
+}
+</style>
