c5e9de78033e0d403a70566236d555f684051aa3
dzyanino <dzyanino@gmail.com>
2026-05-03 22:08:55 +0300

    feat(components): made the url dynamic by using a function and better layout?

--- app/components/Home/CommitsList.vue
+++ app/components/Home/CommitsList.vue
@@ -1,19 +1,25 @@
 <script lang="ts" setup>
-const selectedRepo = inject('selectedRepo', shallowRef<GithubRepo>())
+const selectedRepo = inject('selectedRepo', shallowRef<GitHubRepo>())
 const selectedRepoLoading = inject('selectedRepoLoading', shallowRef<boolean>(false))
 
-const { data: commits, pending: commitsLoading, refresh: refreshCommits } = await useFetch(
-  `/api/github/commits?owner=${selectedRepo.value?.owner.login}&repo=${selectedRepo.value?.name}`,
-  { immediate: false }
+const { data: groupedCommits, refresh: refreshGroupedCommits } = await useFetch(
+  () => `/api/github/commits?owner=${selectedRepo.value?.owner.login}&repo=${selectedRepo.value?.name}`,
+  {
+    immediate: false,
+
+    onRequest: () => {
+      selectedRepoLoading.value = true
+    },
+
+    onResponse: () => {
+      selectedRepoLoading.value = false
+    }
+  }
 )
 
-watch(selectedRepo, async () => {
-  selectedRepoLoading.value = true
-
-  if (selectedRepo) {
-    await refreshCommits()
-    selectedRepoLoading.value = commitsLoading.value
-  }
+watch(selectedRepo, async (newRepo) => {
+  if (newRepo)
+    await refreshGroupedCommits()
 })
 </script>
 
@@ -27,19 +33,26 @@ watch(selectedRepo, async () => {
     />
     <div
       v-else
-      class="flex flex-col gap-2 size-full"
+      class="flex flex-col size-full"
     >
       <template
-        v-for="commit in commits"
-        :key="commit.sha"
+        v-for="group in groupedCommits"
+        :key="group.date"
       >
-        <UAlert
-          :title="commit.commit.message"
-          :description="commit.sha"
-          color="neutral"
-          variant="soft"
-          class="min-h-20"
-        />
+        <UCard
+          :title="group.date"
+        >
+          <template
+            v-for="commit in group.commits"
+            :key="commit.sha"
+          >
+            <span>{{ commit.message }}</span>
+            <span>{{ commit.sha }}</span>
+            <span>{{ commit.author }}</span>
+            <span>{{ commit.html_url }}</span>
+            <span>{{ commit.sha }}</span>
+          </template>
+        </UCard>
       </template>
     </div>
   </div>
