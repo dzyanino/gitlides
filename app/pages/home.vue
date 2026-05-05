b0dc6652a8c3925ea10caca917533d73415a2f6c
dzyanino <dzyanino@gmail.com>
2026-05-05 11:46:44 +0300

    feat(pages): better layout with sidebar

--- app/pages/home.vue
+++ app/pages/home.vue
@@ -7,6 +7,9 @@ const viewport = useViewport()
 
 const toast = useToast()
 
+/**
+ * Data
+ */
 const { data: savedRepos, refresh: refreshSavedRepos } = await useFetch('/api/github/saved-repo')
 
 const { data: repos, pending: reposLoading, refresh: refreshRepos } = await useFetch('/api/github/repos', {
@@ -14,10 +17,18 @@ const { data: repos, pending: reposLoading, refresh: refreshRepos } = await useF
   key: 'repos'
 })
 
+/**
+ * UI state variables
+ */
+const isSidebarOpen = shallowRef<boolean>(false)
+
 const selectedRepos = ref<Tables<'repo'>[]>([])
 
 const isAddReposModalOpen = shallowRef<boolean>(false)
 
+/**
+ * Functions
+ */
 async function getRepos() {
   isAddReposModalOpen.value = true
 
@@ -81,22 +92,77 @@ provide('selectedRepoLoading', selectedRepoLoading)
         class="max-w-md mx-auto self-center -mt-(--ui-header-height)"
       />
 
-      <div
-        v-else
-        class="flex flex-col md:flex-row w-full h-[calc(100vh-var(--ui-header-height))] py-4"
-      >
-        <div class="flex flex-col w-full md:max-w-4/11 h-full max-h-fit md:max-h-full gap-2 p-2">
-          <HomeRepoDetails />
+      <template v-else>
+        <div v-show="viewport.isLessThan('tablet')">
+          <USidebar
+            v-model:open="isSidebarOpen"
+            mode="drawer"
+            :style="{ '--sidebar-width': '28rem' }"
+          >
+            <div
+              class="flex flex-col gap-4"
+            >
+              <HomeRepoDetails />
+              <HomeRepoSelect />
+            </div>
 
-          <HomeRepoSelect v-show="viewport.isLessThan('tablet')" />
-
-          <HomeReposList v-show="viewport.isGreaterOrEquals('tablet')" />
+            <template #close>
+              {{}}
+            </template>
+          </USidebar>
         </div>
 
-        <div class="flex flex-col w-full h-full max-h-full overflow-auto wrap-break-word gap-2 p-2">
-          <HomeCommitsList />
+        <div
+          class="flex flex-col md:flex-row w-full h-[calc(100vh-var(--ui-header-height))] md:py-4"
+        >
+          <div
+            v-show="viewport.isGreaterOrEquals('tablet')"
+            class="flex flex-col w-full max-w-4/11 h-full max-h-full gap-2 p-2"
+          >
+            <div class="flex w-full items-center justify-end gap-2">
+              <UButton
+                icon="i-lucide-folder-plus"
+                variant="subtle"
+              >
+                Add
+              </UButton>
+            </div>
+
+            <HomeRepoDetails />
+
+            <HomeReposList />
+          </div>
+
+          <div class="flex flex-col w-full h-full max-h-full overflow-auto wrap-break-word gap-2 p-2">
+            <div class="flex w-full items-center justify-between md:justify-end gap-2">
+              <UButton
+                v-if="viewport.isLessThan('tablet')"
+                icon="i-lucide-panel-left"
+                color="neutral"
+                variant="ghost"
+                @click="isSidebarOpen = !isSidebarOpen"
+              />
+
+              <div class="flex items-center gap-2">
+                <UButton
+                  icon="i-lucide-chevron-left"
+                  variant="ghost"
+                >
+                  Previous
+                </UButton>
+                <UButton
+                  icon="i-lucide-chevron-right"
+                  variant="ghost"
+                >
+                  Next
+                </UButton>
+              </div>
+            </div>
+
+            <HomeCommitsList />
+          </div>
         </div>
-      </div>
+      </template>
     </UContainer>
 
     <UModal
